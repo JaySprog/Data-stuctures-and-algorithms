ab657d69b64a659ba4c363be5692e9eef153bebd
JaySprog <100353545+JaySprog@users.noreply.github.com>
2022-03-01 13:57:50 +0530

    Add files via upload
    deletes entire linkedlist

---
+++ LL_delete_linkedlist.py
@@ -0,0 +1,42 @@
+class node:
+    def __init__(self,x):
+        self.val=x
+        self.nf=None
+
+class linkedlist:
+    def __init__(self):
+        self.whole=None
+
+    def add(self,x):
+        node_struct=node(x)
+
+        if self.whole == None:
+            self.whole=node_struct
+            return
+        
+        temp=self.whole
+        while temp.nf!=None:
+            temp=temp.nf
+        temp.nf=node_struct
+
+    def printvalue(self):
+        temp=self.whole
+        while temp is not None:
+            print(temp.val)
+            temp=temp.nf
+
+
+    def delete(self):
+        self.whole=None
+        
+    
+
+a=linkedlist()
+a.add(8)
+a.add(2)
+a.add(3)
+a.add(1)
+a.add(7)
+a.printvalue()
+a.delete()
+a.printvalue()
